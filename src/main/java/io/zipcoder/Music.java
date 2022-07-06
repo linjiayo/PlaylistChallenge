1ac3ac8d9cd6fd30e184fdeed35f1d39233aef89
Jiayong <jiagains@gmail.com>
2022-07-06 13:21:21 -0400

    add method

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,29 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int countL = 0;
+        int countR = 0;
+        int l = startIndex;
+        int r = startIndex;
+
+        for (int i = 0; i < playList.length; i++) {
+            if (this.playList[l].equals(selection)) {
+                return countL;
+            } else if (this.playList[r].equals(selection)) {
+                return countR;
+            }
+            l--;
+            r++;
+            if (l < 0) {
+                l = playList.length - 1;
+            }
+            if (r > playList.length -1); {
+                r = 0;
+            }
+            countR++;
+            countL++;
+
+        }
+        return 0;
     }
 }
